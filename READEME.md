08c1d92d8d1f2183f816ace199a1b4fcef0fb8ea
immary <1069865896@qq.com>
2018-04-21 18:33:55 +0800

    parseXML by DOM

--- READEME.md
+++ READEME.md
@@ -1,1 +1,6 @@
 ### 记录Java解析XML的方式
+
+1. DOM解析
+2. SAX解析
+3. JDOM解析
+4. DOM4J解析
---
+++ src/main/java/com/chenmr/parseXML/DOM/ParesXML.java
@@ -0,0 +1,7 @@
+package com.chenmr.parseXML.DOM;
+
+/**
+ * Created by Chenmr on 2018/4/21/021.
+ */
+public class ParesXML {
+}
